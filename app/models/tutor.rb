6a34ca1822842e7169543f72867969476d9594cf
arthur318 <arthurhwang318@gmail.com>
2020-06-01 08:46:50 -0700

    don

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,3 +1,37 @@
+
 class Tutor
-  
+    attr_accessor :name
+    @@all = []
+
+    #`Tutor` is initialized with a name (string)
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    #returns an array of all the `Tutor` instances that have been initialized
+    def self.all
+        @@all
+    end
+
+    # returns an array of `Course` instances associated with the `Tutor` instance.
+    def courses
+        Course.all.select do |course| 
+            course.tutor == self
+        end
+    end
+    
+    # returns an array of `Student` instances tutored by the `Tutor` instance.
+    def students
+        courses.map do |course| 
+            course.student
+        end
+    end
+
+    #returns `true` if the `Tutor` has tutored this `Student` (if there is a `Course` instance that has this `Tutor` and `Student`), returns `false` otherwise
+    def tutored_student(student)
+		students.include? student
+	end
+
+
 end
