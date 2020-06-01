76339563997a3bbaa5ead818f51bdc8bb94f9df1
Eric Ou <eric.ou@yale.edu>
2020-06-01 07:58:38 -0700

    done

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,24 @@
 class Student
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
 
+    def courses()
+        Course.all().select{|course| course.student == self}
+    end
+
+    def tutors()
+        courses().map{|course| course.tutor}
+    end
+
+    def enrolled_fulltime?
+        courses().count() >= 3
+    end
+
+    def self.all()
+        @@all
+    end
 end
--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,3 +1,24 @@
 class Tutor
-  
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def courses()
+        Course.all().select{|course| course.tutor == self}
+    end
+
+    def students()
+        courses().map{|course| course.student}
+    end
+
+    def tutored_student?(student)
+        students().include?(student)
+    end
+
+    def self.all()
+        @@all
+    end
 end
