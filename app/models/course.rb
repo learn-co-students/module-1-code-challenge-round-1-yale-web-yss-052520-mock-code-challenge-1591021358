5c11120618baf56ee82539bf70ab27818b4c9e70
Drew Beckmen <drew.beckmen@yale.edu>
2020-06-01 11:20:08 -0400

    built out OO relationships for coding challenge

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,17 @@
 class Course
 
+    attr_reader :course_name, :student, :tutor
+    @@all = []
+
+    def initialize(tutor, student, course_name)
+        @tutor = tutor
+        @student = student
+        @course_name = course_name
+        self.class.all << self 
+    end
+
+    # Class method to return all instances of this class that have been created
+    def self.all 
+        @@all 
+    end 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,30 @@
+require_relative './modules.rb'
+
 class Student
 
+    # Code for initialize method was refactored into a module because it was identitcal to initialize method in tutor.rb
+    include Initializer::InstanceMethods
+
+    attr_accessor :name 
+    @@all = []
+
+    def self.all 
+        @@all 
+    end
+
+    # Return an array of all Course instances for the Student 
+    def courses 
+        Course.all.select {|course| course.student == self }
+    end 
+
+    # Return an array of aall the Tutor instances that are tutoring the Student 
+    # Tutor instances may not be unique if a student has the same Tutor for a different course
+    def tutors 
+        self.courses.map {|course| course.tutor}
+    end 
+
+    # Returns true if the Student has enrolled for at least 3 courses, false otherwise
+    def enrolled_fulltime?
+        self.courses.size >= 3 #utilizes previously written instance method
+    end 
 end
