4700eda2ed3faee338e191b0d463e6ae94fb5541
robertalopez <roblop1234@yahoo.com>
2020-06-01 08:13:05 -0700

    mock-challenge

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,29 @@
 class Student
 
+    @@all = []
+
+    attr_accessor :name 
+
+    def initialize(name)
+        @name = name 
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all
+    end 
+
+    def courses
+        Course.all.select {|course| course.student == self}
+    end 
+
+    def tutors 
+        self.courses.map {|course| course.tutor}
+    end 
+
+    def enrolled_fulltime?
+        self.courses.count > 2 
+    end 
+
+
 end
