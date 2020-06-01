a2472fee980e35a542237422d4dc1fd52b3cb417
davidyu7 <david.yu@yale.edu>
2020-06-01 11:16:36 -0400

    finished code challenge

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,3 +1,32 @@
 class Tutor
-  
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def courses
+        Course.all.select{|course| course.tutor == self}
+    end
+
+    def students
+        # I am assuming that the specs are looking for a unique list -
+        # if a tutor is tutoring one student but in two classes, 
+        # they still only have one student, not two, 
+        # so the length of their .students method should be 1.
+        self.courses.map{|course| course.student}.uniq
+    end
+
+    def tutored_student?(student)
+        self.students.any?(student)
+    end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -10,7 +10,26 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+alex = Student.new("Alex")
+ben = Student.new("Ben")
+cat = Student.new("Cat")
+dave = Student.new("Dave")
 
+xiggins = Tutor.new("Xiggins")
+yiggins = Tutor.new("Yiggins")
+ziggins = Tutor.new("Ziggins")
+
+chem_xa = Course.new(xiggins, alex, "Chemistry")
+chem_xb = Course.new(xiggins, ben, "Chemistry")
+math_xa = Course.new(xiggins, alex, "Math")
+math_xc = Course.new(xiggins, cat, "Math")
+math_yd = Course.new(yiggins, dave, "Math")
+math_yb = Course.new(yiggins, ben, "Math")
+bio_yd = Course.new(yiggins, dave, "Bio")
+bio_yc = Course.new(yiggins, cat, "Bio")
+eng_zd = Course.new(ziggins, dave, "English")
+eng_zc = Course.new(ziggins, cat, "English")
+hist_zc = Course.new(ziggins, cat, "History")
 
 
 
