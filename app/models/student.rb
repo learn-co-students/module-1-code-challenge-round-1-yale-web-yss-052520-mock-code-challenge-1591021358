05113174862b9468f88d28c44a7c45d906f62f74
tbartelsmeyer <47281160+tbartelsmeyer@users.noreply.github.com>
2020-06-01 11:04:36 -0500

    code-challenge-1-submission

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,39 @@
 class Student
 
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def courses
+    Course.all.select { |course| course.student.name == @name }
+  end
+
+  def tutors
+    self.courses.map { |course| course.tutor }.uniq
+  end
+
+  def enrolled_fulltime?
+    self.courses.length >= 3 ? true : false
+  end
+
 end
+
+=begin
+`Student#initialize(name)`
+  `Student` is initialized with a name (string)
+  name **can be** changed after the `Student` is initialized
+`Student#name`
+  returns the `Student`'s name
+`Student.all`
+  returns an array of all the `Student` instances that have been initialized
+=end
