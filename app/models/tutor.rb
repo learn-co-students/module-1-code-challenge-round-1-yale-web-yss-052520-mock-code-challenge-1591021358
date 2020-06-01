57042b43e5ca72dc51bba1beb3b9dcc0d05357df
ev275 <enrique.valencia@yale.edu>
2020-06-01 10:16:02 -0500

    done

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,3 +1,29 @@
 class Tutor
-  
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+    
+    def courses
+        Course.all.select do |course|
+            course.tutor == self
+        end
+    end
+
+    def students
+        courses.map do |course|
+            course.student
+        end
+    end
+
+    def tutored_student?(student)
+        students.any?(student)
+    end            
 end
