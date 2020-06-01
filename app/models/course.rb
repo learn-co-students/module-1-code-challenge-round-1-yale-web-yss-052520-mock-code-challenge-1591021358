423b0219b19898e01d2658c9bdf984b24d61dc67
Jonkavaliauskas <jonas.kavaliauskas@yale.edu>
2020-06-01 19:19:55 +0300

    Done

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,28 @@
 class Course
+    attr_accessor :tutor, :student, :course_name
+
+    @@all = []
+
+    def initialize(tutor, student, course_name)
+        @tutor = tutor
+        @student = student
+        @course_name = course_name
+        @@all << self
+        #Tutor.all << tutor
+        
+    end
+
+    def self.all
+        @@all
+    end
+
+    # def tutor
+    #     self.tutor
+    # end
+
+    # def student
+    #     self.student
+    # end
 
 end
+
