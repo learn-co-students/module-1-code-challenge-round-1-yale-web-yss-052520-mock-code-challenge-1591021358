d298c28fc2f6390cf749783c404f202c4c9008ff
isabelxklee <hello@isabelklee.co>
2020-08-10 12:19:12 -0400

    added timeline

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,3 +1,51 @@
 class Tutor
-  
+
+    attr_accessor :name
+    @@all = []
+    
+    def initialize(name_arg)
+        @name = name_arg
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def courses 
+        Course.all.select do |course|
+            course.tutor == self
+        end
+    end
+
+    # def students
+    #     students_array = courses.map do |course|
+    #         course.student
+    #     end
+
+    #     students_array.uniq
+    # end
+
+    def students
+        courses.map { |course| course.student }.uniq
+    end
+
 end
+
+# TESTS
+
+# - `Tutor#initialize(name)`
+#   - `Tutor` is initialized with a name (string)
+#   - name **can be** changed after the Tutor is initialized
+
+# - `Tutor#name`
+#   - returns the Tutor's name
+
+# - `Tutor.all`
+#   - returns an array of all the `Tutor` instances that have been initialized
+
+# - `Tutor#courses`
+#   - returns an array of `Course` instances associated with the `Tutor` instance.
+
+# - `Tutor#students`
+#   - returns an array of `Student` instances tutored by the `Tutor` instance.
--- tools/console.rb
+++ tools/console.rb
@@ -11,7 +11,21 @@ end
 # so they will be available to test and play around with in your console
 
 
+isabel = Student.new("isabel")
+frankie = Student.new("frankie")
+celeste = Student.new("celeste")
 
+teacher_a = Tutor.new("teacher a")
+teacher_b = Tutor.new("teacher b")
+teacher_c = Tutor.new("teacher c")
+
+math = Course.new(teacher_a, isabel, "Math")
+bio = Course.new(teacher_a, isabel, "Biology")
+chem = Course.new(teacher_a, celeste, "Chemistry")
+art_history = Course.new(teacher_b, frankie, "Art History")
+illustration = Course.new(teacher_b, celeste, "Illustration")
+design = Course.new(teacher_c, isabel, "Design")
+science = Course.new(teacher_c, celeste, "Science")
 
 
 # ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
