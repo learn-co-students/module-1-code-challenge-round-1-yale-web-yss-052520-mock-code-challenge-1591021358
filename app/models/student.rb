849584451c6d3cda1692eef5659b7da0f155a2c6
isabelxklee <hello@isabelklee.co>
2020-08-10 12:29:27 -0400

    back to master

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,28 +1,2 @@
 class Student
-
-    attr_accessor :name
-
-    @@all = []
-
-    def initialize(name_arg)
-        @name = name_arg
-        @@all << self
-    end
-
-    def self.all
-        @@all
-    end
-
 end
-
-# TESTS
-
-# - `Student#initialize(name)`
-#   - `Student` is initialized with a name (string)
-#   - name **can be** changed after the `Student` is initialized
-
-# - `Student#name`
-#   - returns the `Student`'s name
-
-# - `Student.all`
-#   - returns an array of all the `Student` instances that have been initialized
--- tools/console.rb
+++ tools/console.rb
@@ -10,24 +10,6 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
-
-isabel = Student.new("isabel")
-frankie = Student.new("frankie")
-celeste = Student.new("celeste")
-
-teacher_a = Tutor.new("teacher a")
-teacher_b = Tutor.new("teacher b")
-teacher_c = Tutor.new("teacher c")
-
-math = Course.new(teacher_a, isabel, "Math")
-bio = Course.new(teacher_a, isabel, "Biology")
-chem = Course.new(teacher_a, celeste, "Chemistry")
-art_history = Course.new(teacher_b, frankie, "Art History")
-illustration = Course.new(teacher_b, celeste, "Illustration")
-design = Course.new(teacher_c, isabel, "Design")
-science = Course.new(teacher_c, celeste, "Science")
-
-
 # ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
 binding.pry
 0
